4ae2db3c8ee639fb4bd2276c91503db5f66d191a
Jacob Ider Chitham <jacobic@mpe.mpg.de>
2018-10-28 00:37:53 +0200

    moved classes to objects

--- spiders/utils/misc.py
+++ spiders/utils/misc.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+import numpy as np
 import tqdm
 import multiprocess as mp
 
@@ -11,58 +13,11 @@ def file_size(path, unit='B'):
     n = {'B': 0, 'KB': 1, 'MB': 2}
     return size / pow(1024, n[unit])
 
-@dataclass
-class FileBase:
-    """
-    Parameters
-    ----------
-    name : str
-        Partial file name, includes extension but excludes path to file.
-    """
-    name : str
-    subdir :str
-    abspath : str
-
-    def __post_init__(self):
-        self.size = file_size(self.abspath, 'MB')
-        self.exists = os.path.isfile(self.abspath)
-
-    def _size(self, unit='MB'):
-        return file_size(self.abspath, unit)
-
-class Str(str):
-    """
-    Subclass that extends the format string for strings to begin
-    with an optional 'u' or 'l' to shift to upper or lower case.
-    """
-
-    def __format__(self, fmt):
-        """
-        Parameters
-        ----------
-        fmt : {'u', 'l'}
-            Upper or Lower
-        """
-        s = str(self)
-        if fmt[0] == 'u':
-            s = self.upper()
-            fmt = fmt[1:]
-        elif fmt[0] == 'l':
-            s = self.lower()
-            fmt = fmt[1:]
-        return s.__format__(fmt)
-
-    def __getitem__(self, item):
-        """
-        Returns
-        ----------
-        The item as the form of another Str object.
-        """
-        return Str(str(self).__getitem__(item))
-
 
 def assert_arraylike(x):
-    # Ensure that x is array-like
+    """
+    Ensure that x is array-like
+    """
     if isinstance(x, (int, float)):
         x = [x]
     # Ensure that x is a numpy array as np.where is used in this function.
@@ -74,7 +29,7 @@ def assert_arraylike(x):
     return x
 
 
-def parallelize(func, inputs, n_proc, sync=True, async=False, loglevel=None):
+def parallelize(func, inputs, n_proc, loglevel=None):
     """
     Helper function to parrallelise any mappable function either
     synchously or asynchrously using python's multiprocessing module.
