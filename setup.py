de83997ba75c618e780c9a335623a39adb0624cb
Jacob Ider Chitham <jacobic@mpe.mpg.de>
2019-04-24 13:14:04 +0200

    + requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', 'colossus', 'astropy', 'pandas', 'scikit-learn',
+                'numpy', 'scipy', 'dill', 'emcee', 'tqdm', 'multiprocess',
+                'dask', 'pyspark'
+
+                ]
 
 setup_requirements = ['pytest-runner', ]
 
