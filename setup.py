bb1755d792be845c8461d3ba2efc905f5ab80864
Jacob Ider Chitham <jacobic@mpe.mpg.de>
2019-04-24 14:28:28 +0200

    removed dependency links

--- setup.py
+++ setup.py
@@ -14,16 +14,15 @@ with open('HISTORY.rst') as history_file:
 requirements = ['Click>=6.0', 'colossus', 'astropy', 'pandas', 'scikit-learn',
                 'numpy', 'scipy', 'dill', 'emcee', 'tqdm', 'multiprocess',
                 'dask', 'pyspark'
-
                 ]
 
-dependency_links = [
+# dependency_links = [
     # Make sure to include the `#egg` portion so the `install_requires`
     # recognizes the package
     # Dask friendly version of emcee.
-    'git+ssh://git@github.com/jacobic/emcee.git#egg=emcee-emcee-2.2.1'
-
-]
+    # 'git+ssh://git@github.com/jacobic/emcee.git#egg=emcee-emcee-2.2.1'
+#
+# ]
 
 setup_requirements = ['pytest-runner', ]
 
