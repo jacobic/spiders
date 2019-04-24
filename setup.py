9450bd2489a000632d260e576f2505ca9489d532
Jacob Ider Chitham <jacobic@mpe.mpg.de>
2019-04-24 13:35:19 +0200

    + dask friendly emcee dependency

--- setup.py
+++ setup.py
@@ -17,43 +17,37 @@ requirements = ['Click>=6.0', 'colossus', 'astropy', 'pandas', 'scikit-learn',
 
                 ]
 
+dependency_links = [
+    # Make sure to include the `#egg` portion so the `install_requires`
+    # recognizes the package
+    # Dask friendly version of emcee.
+    'git+ssh://git@github.com/jacobic/emcee.git#egg=emcee-emcee-2.2.1'
+
+]
+
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
 
-setup(
-    author="Jacob Ider Chitham",
-    author_email='jacobic@mpe.mpg.de',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+setup(author="Jacob Ider Chitham", author_email='jacobic@mpe.mpg.de',
+    classifiers=['Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License', 'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+        'Programming Language :: Python :: 3.7', ],
+    description="Python Boilerplate contains all the boilerplate you need to "
+                "create a Python package.",
     entry_points={
-        'console_scripts': [
-            'spiders=spiders.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='spiders',
-    name='spiders',
+        'console_scripts': ['spiders=spiders.cli:main', ], },
+    install_requires=requirements, license="MIT license",
+    long_description=readme + '\n\n' + history, include_package_data=True,
+    keywords='spiders', name='spiders',
     packages=find_packages(include=['spiders']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/jacobic/spiders',
-    version='0.1.0',
-    zip_safe=False,
-)
+    setup_requires=setup_requirements, test_suite='tests',
+    tests_require=test_requirements, url='https://github.com/jacobic/spiders',
+    version='0.1.0', zip_safe=False, )
